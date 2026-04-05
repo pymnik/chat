a60eabb703dbfa1117c7f0ef5c098e8a2ed0ac43
Nikita <nikita.pimenov258@gmail.com>
2026-04-05 12:37:45 +0400

    db conn fix

--- db.py
+++ db.py
@@ -2,7 +2,8 @@ import psycopg2
 import os
 import sqlalchemy
 
-conn = psycopg2.connect(dbname='db_vuci', user='db_vuci_user', password=os.environ.get('DATABASE_PASSWORD'), host=os.environ.get('DATABASE_URL'), port=5432)
+# conn = psycopg2.connect(dbname='db_vuci', user='db_vuci_user', password=os.environ.get('DATABASE_PASSWORD'), host=os.environ.get('DATABASE_URL'), port=5432)
+conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
 cur = conn.cursor()
 
 # with psycopg2.connect(os.environ.get('DATABASE_URL'))
