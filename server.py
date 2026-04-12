85f3e8ffd2f19b4fb663fb2bf38ed18ada988890
Nikita <nikita.pimenov258@gmail.com>
2026-04-12 12:40:24 +0400

    chat requests from server

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import os
 import psycopg2
 import db
+import json
 
 conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
 cur = conn.cursor()
@@ -21,6 +22,15 @@ def send_message():
     username = message.get("username")
     id = message.get("id")
     return True
+
+@app.route("/api/chat", methods=["GET"])
+def get_chats(user_id):
+    chats = cur.execute(f"SELECT CHATS.id, CHATS.user1, CHATS.user2, U1.username as U1NAME, U2.username as U2NAME FROM WHERE user1 = {user_id} OR user2 = {user_id} '\
+                         LEFT JOIN USERS U1 ON U1.id = CHATS.user1 LEFT JOIN USERS U2 ON U2.id = CHATS.user2")
+    chats = chats.fetchall()
+    chats = json.dumps(chats)
+    return chats
+    
 
 if __name__ == "__main__":
     app.run()
