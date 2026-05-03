6b77a66a5e561d9f35d2c3a997262960de25eda6
Nikita <nikita.pimenov258@gmail.com>
2026-05-03 11:50:31 +0400

    supabase library

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
-import psycopg2
 import json
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-cur = conn.cursor()
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_KEY")
+)
+            
 @app.route("/api/messages", methods=["GET"])
 def get_messages():
     return None
@@ -24,11 +29,13 @@ def send_message():
 
 @app.route("/api/chats/<user_id>", methods=["GET"])
 def get_chats(user_id):
-    chats = cur.execute(f"SELECT CHATS.id, CHATS.user1, CHATS.user2, U1.username as U1NAME, U2.username as U2NAME FROM CHATS LEFT JOIN USERS U1 ON U1.id = CHATS.user1 LEFT JOIN USERS U2 ON U2.id = CHATS.user2 WHERE user1 = 0 OR user2 = 1")
-    chats = chats.fetchall()
+    chats = supabase.execute(f"SELECT CHATS.id, CHATS.user1, CHATS.user2, U1.username as U1NAME, U2.username as U2NAME FROM CHATS LEFT JOIN USERS U1 ON U1.id = CHATS.user1 LEFT JOIN USERS U2 ON U2.id = CHATS.user2 WHERE user1 = 0 OR user2 = 1")
+    chats = chats.data()
     chats = json.dumps(chats)
+    print(chats)
     return chats
     
 
 if __name__ == "__main__":
+    print(get_chats(0))
     app.run()
