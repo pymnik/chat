83aff0e9910d88192a71d5aa2ad276f340216ddc
Nikita <nikita.pimenov258@gmail.com>
2026-06-21 10:23:37 +0200

    bool jsonify

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def get_chats(user_id):
             "user_id_input":int(user_id)
         }
     ).execute()
-    return {"sent": response.data}
+    return jsonify({"sent": str(response.data)})
 
 @app.route("/api/auth/<name>", methods=["GET"])
 def auth(name):
