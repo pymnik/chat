dd9cd255eef26a21d0f512b5bf10eac905764f06
Nikita <nikita.pimenov258@gmail.com>
2026-08-09 10:27:42 +0200

    fix for converting 3

--- server.py
+++ server.py
@@ -35,22 +35,33 @@ def send_message(msg):
     if "file" in request.files:
         file = request.files["file"]
         file_extension = os.path.splitext(file.filename)[1].lower()
-        print("File extension:", file_extension)
-        if file_extension in videoTypes:
-            ffmpeg.input(file).output("temp.mov").run(overwrite_output=True)
-            file_type = "mov"
+        upload_filename = file.filename
+        content_type = file.content_type
+        if file_extension in videoTypes or file_extension in audioTypes:
+            temp_input = "temp_input" + file_extension
+            file.save(temp_input)
+            if file_extension in videoTypes:
+                file_type = "mov"
+                content_type = "video/quicktime"
+            else:
+                file_type = "wav"
+                content_type = "audio/wav"
+            ffmpeg.input(temp_input).output("temp." + file_type).run(overwrite_output=True)
+            os.remove(temp_input)
             converted = open("temp." + file_type, "rb")
-        elif file_extension in audioTypes:
-            ffmpeg.input(file).output("temp.wav").run(overwrite_output=True)
-            file_type = "wav"
-            converted = open("temp." + file_type, "rb")
+            upload_filename = os.path.splitext(file.filename)[0] + "." + file_type
+            file_bytes = converted.read()
+            converted.close()
+            os.remove("temp." + file_type)
+        else:
+            file_bytes = file.read()
 
         supabase.storage.from_("chat_files").upload(
-            path=f"chat_files/{file.filename}",
-            file=converted.read(),
-            file_options={"content-type": file.content_type, "upsert": "true"}
+            path=f"chat_files/{upload_filename}",
+            file=file_bytes,
+            file_options={"content-type": content_type, "upsert": "true"}
         )
-        public_url = supabase.storage.from_("chat_files").get_public_url(f"chat_files/{file.filename}")
+        public_url = supabase.storage.from_("chat_files").get_public_url(f"chat_files/{upload_filename}")
         msg["file_url"] = public_url
 
     response = supabase.rpc(
